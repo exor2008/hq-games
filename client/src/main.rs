5d61f913a28e8789467f648ad997df282b6e6b17
Ian <exor2008@yandex.ru>
2025-01-01 22:28:33 -0500

    Client initialized

---
+++ client/src/main.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins((DefaultPlugins,))
+        .add_systems(Startup, setup);
+    app.run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn(Text2d::new("Hello!"));
+}
